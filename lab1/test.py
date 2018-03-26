87980af977416beaaf49148623dd4483c53675e0
yonk <nopctoday@gmail.com>
2018-03-26 19:03:39 +0200

    hamming is not working

--- lab1/test.py
+++ lab1/test.py
@@ -4,14 +4,17 @@ import matplotlib.pyplot as plt
 data = np.load('lab1_data.npz')['data']
 example_data = np.load('lab1_example.npz')['example'].item()
 
-plt.plot(example_data['samples'], linewidth=0.5)
+#plt.plot(example_data['samples'], linewidth=0.5)
 output = proto.mfcc(example_data['samples'])
-if np.array_equal(output, example_data['preemph']):
+if np.array_equal(output, example_data['windowed']):
     print('it works!')
 
-plt.figure()
-plt.pcolormesh(example_data['preemph'].T)
-plt.show()
+
+
+plt.subplot(2, 1, 1)
+plt.title('windowed')
+plt.pcolormesh(example_data['windowed'].T)
+plt.subplot(2, 1, 2)
 plt.pcolormesh(output.T)
 plt.show()
 
