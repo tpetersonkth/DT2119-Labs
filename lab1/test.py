ef794733dff608708724779d134de1cac83b78dc
yonk <nopctoday@gmail.com>
2018-03-26 18:08:21 +0200

    enframe is working

---
+++ lab1/test.py
@@ -0,0 +1,11 @@
+import numpy as np
+from lab1 import proto
+import matplotlib.pyplot as plt
+data = np.load('lab1_data.npz')['data']
+example_data = np.load('lab1_example.npz')['example'].item()
+
+plt.plot(example_data['samples'], linewidth=0.5)
+output = proto.mfcc(example_data['samples'])
+if np.array_equal(output, example_data['frames']):
+    print('it works!')
+print(data)
