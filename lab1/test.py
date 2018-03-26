9dc3b357a35809149217647301370fa65a158dcb
yonk <nopctoday@gmail.com>
2018-03-26 18:33:39 +0200

    preemph working

--- lab1/test.py
+++ lab1/test.py
@@ -6,6 +6,13 @@ example_data = np.load('lab1_example.npz')['example'].item()
 
 plt.plot(example_data['samples'], linewidth=0.5)
 output = proto.mfcc(example_data['samples'])
-if np.array_equal(output, example_data['frames']):
+if np.array_equal(output, example_data['preemph']):
     print('it works!')
+
+plt.figure()
+plt.pcolormesh(example_data['preemph'].T)
+plt.show()
+plt.pcolormesh(output.T)
+plt.show()
+
 print(data)
