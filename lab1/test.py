c0ee47c85948cedc88b63d612df551f6e815378a
yonk <nopctoday@gmail.com>
2018-03-28 14:02:45 +0200

    logmel working

--- lab1/test.py
+++ lab1/test.py
@@ -3,20 +3,20 @@ from lab1 import proto
 import matplotlib.pyplot as plt
 data = np.load('lab1_data.npz')['data']
 example_data = np.load('lab1_example.npz')['example'].item()
-
+target = 'mspec'
 #plt.plot(example_data['samples'], linewidth=0.5)
 output = proto.mfcc(example_data['samples'])
-if np.array_equal(output, example_data['windowed']):
+if np.array_equal(output, example_data[target]):
     print('it works!')
 
 
 
 plt.subplot(2, 1, 1)
 plt.title('Spec')
-plt.pcolormesh(example_data['spec'].T)
+plt.pcolormesh(example_data[target].T)
 plt.subplot(2, 1, 2)
 plt.pcolormesh(output.T)
 plt.show()
 
-diff =  example_data['spec'] - output
+diff =  example_data[target] - output
 print(data)
