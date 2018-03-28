668716eebed0325d80c67769d9f16015d9e314da
pythonpsycho1337 <root@LAPTOP-V8QOMECQ.localdomain>
2018-03-28 16:17:48 +0200

    DTW might be working..

--- lab1/test.py
+++ lab1/test.py
@@ -3,13 +3,12 @@ from lab1 import proto
 import matplotlib.pyplot as plt
 data = np.load('lab1_data.npz')['data']
 example_data = np.load('lab1_example.npz')['example'].item()
+''' 
 target = 'lmfcc'
 #plt.plot(example_data['samples'], linewidth=0.5)
 output = proto.mfcc(example_data['samples'])
 if np.array_equal(output, example_data[target]):
     print('it works!')
-
-
 
 plt.subplot(2, 1, 1)
 plt.title('Spec')
@@ -18,6 +17,12 @@ plt.subplot(2, 1, 2)
 plt.pcolormesh(output.T)
 plt.show()
 
-diff = example_data[target] - output
 
-print(data)
+diff = example_data[target] - output
+'''
+x = example_data['lmfcc']
+#distances = proto.calcDist()
+#output =
+#plt.pcolormesh(output.T)
+#plt.show()
+
