3314ca4c5c3383e899a52a5088684f856ce7909a
yonk <nopctoday@gmail.com>
2018-03-28 14:27:18 +0200

    dct sort of done

--- lab1/test.py
+++ lab1/test.py
@@ -3,7 +3,7 @@ from lab1 import proto
 import matplotlib.pyplot as plt
 data = np.load('lab1_data.npz')['data']
 example_data = np.load('lab1_example.npz')['example'].item()
-target = 'mspec'
+target = 'mfcc'
 #plt.plot(example_data['samples'], linewidth=0.5)
 output = proto.mfcc(example_data['samples'])
 if np.array_equal(output, example_data[target]):
