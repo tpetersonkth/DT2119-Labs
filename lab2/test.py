002d216c57f8069f914a4b8c906a222cbbefc75f
yonk <nopctoday@gmail.com>
2018-04-24 21:16:56 +0200

    cool stuff

--- lab2/test.py
+++ lab2/test.py
@@ -49,7 +49,7 @@ log_gamma = proto2.statePosteriors(example_data['logalpha'],example_data['logbet
 diffg = log_gamma - example_data['loggamma']
 
 log_gamma = proto2.updateMeanAndVar(lmfcc,example_data['loggamma'])
-diffg = log_gamma - ref
+
 
 
 #Print execution time
