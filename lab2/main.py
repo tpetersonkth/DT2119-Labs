fe47b670690ff6e4ce57a4bffc3736968f0cbac9
pythonpsycho1337 <root@LAPTOP-V8QOMECQ.localdomain>
2018-04-25 11:47:25 +0200

    Added code for stepwise plotting and changed name of test.py to main.py

--- lab2/main.py
+++ lab2/main.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import lab2.proto2 as proto2
 import lab2.tools2 as tools2
+import lab2.plotting as plotting
 from lab2.prondict import prondict
 from timeit import default_timer as timer
 
@@ -21,13 +22,12 @@ for digit in prondict.keys():
     modellist[digit] = ['sil'] + prondict[digit] + ['sil']
 
 #Generate HMM for the word zero
-hmmTest = proto2.concatHMMs(phoneHMMs,modellist['9'])
+hmmTest = proto2.concatHMMs(phoneHMMs,modellist['o'])
 log_startprob = np.log(hmmTest['startprob'])
 log_trans = np.log(hmmTest['transmat'])[:-1, :-1]
 
-
-
-proto2.baum_welch(lmfcc_example, hmmTest['means'], hmmTest['covars'], log_startprob, log_trans, example_data)
+#Temporary testing
+#proto2.baum_welch(lmfcc_example, hmmTest['means'], hmmTest['covars'], log_startprob, log_trans, example_data)
 
 loglikelihood = tools2.log_multivariate_normal_density_diag(lmfcc_example, hmmTest['means'], hmmTest['covars'])
 
@@ -40,45 +40,20 @@ viter = proto2.viterbi(loglikelihood, log_startprob ,log_trans)
 diffv1 = viter[0] - example_data['vloglik'][0]
 diffv2 = viter[1] - example_data['vloglik'][1]
 
-
 #Backward alogithm
 log_beta = proto2.backward(loglikelihood, log_startprob ,log_trans)
 diffb = log_beta - example_data['logbeta']
 
 #State Posteriors
 log_gamma = proto2.statePosteriors(example_data['logalpha'],example_data['logbeta'])
+log_gamma = proto2.statePosteriors(log_alpha,log_beta)
 diffg = log_gamma - example_data['loggamma']
-
-log_gamma = proto2.updateMeanAndVar(lmfcc_example, example_data['loggamma'])
-
-
 
 #Print execution time
 print('Execution done in '+str(round((timer()-startTime),2))+" seconds")
 
 #Plot graphs
-plt.figure(figsize=(20,10))
-ax = plt.subplot(1, 1, 1)
-ax.set_title('lmfcc')
-ax.set_yticklabels([])
-ax.set_xticklabels([])
-plt.pcolormesh(np.transpose(lmfcc_example))
-plt.show()
-
-plt.figure(figsize=(20,10))
-ax = plt.subplot(1, 1, 1)
-plt.title('lmfcc')
-ax.set_yticklabels([])
-ax.set_xticklabels([])
-plt.pcolormesh(np.transpose(lmfcc_example))
-plt.show()
-
-plt.figure(figsize=(20,10))
-ax = plt.subplot(1, 1, 1)
-ax.set_title('log alpha')
-ax.set_yticklabels([])
-ax.set_xticklabels([])
-plt.pcolormesh(np.transpose(log_alpha))
+plt = plotting.plotEachStep(lmfcc_example,loglikelihood,log_alpha,log_beta,log_gamma)
 plt.show()
 
 
