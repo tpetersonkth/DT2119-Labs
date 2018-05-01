665cbbf31480b704fea0778855c59ea350d878e9
yonk <nopctoday@gmail.com>
2018-05-01 14:11:05 +0200

    adding lmfcc

--- lab3/main.py
+++ lab3/main.py
@@ -2,6 +2,8 @@ import lab3.lab3_proto
 import numpy as np
 import pickle
 
+from lab3.lab3_tools import *
+from lab1.proto import mfcc
 #Get stateList
 '''
 phoneHMMs = np.load('lab2_models.npz')['phoneHMMs'].item()
@@ -13,7 +15,12 @@ stateList = [ph + '_' + str(id) for ph in phones for id in range(nstates[ph])]
 #    pickle.dump(stateList, f)
 '''
 
-with open('stateList.pkl', 'rb') as f:
+with open('lab3/stateList.pkl', 'rb') as f:
     stateList = pickle.load(f)
+
+fname = 'lab3/asset/tidigits/disc_4.1.1/tidigits/train/man/nw/z43a.wav'
+samples, samplingrate = loadAudio(fname)
+lmfcc = mfcc(samples)
+
 
 print("Done")
