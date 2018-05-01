b0596dfe56ec799cfc26bbe27aee657024c4d9c6
pythonpsycho1337 <root@LAPTOP-V8QOMECQ.localdomain>
2018-05-01 14:00:34 +0200

    lmfcc computations added

--- lab3/test.py
+++ lab3/test.py
@@ -1,5 +1,6 @@
-import lab3_tools as tools
+import lab3.lab3_tools as tools
+import lab1.proto as lab1Proto
 
-tools.loadAudio("1a.wav")
-
+samples, samplingrateR = tools.loadAudio("1a.wav")
+lmfcc = lab1Proto.mfcc(samples,samplingrate=samplingrateR)
 print("Done")
