b419a1b27166f84adf898cd0346cbd33a672872d
pythonpsycho1337 <root@LAPTOP-V8QOMECQ.localdomain>
2018-05-01 14:00:34 +0200

    lmfcc computations added

--- lab3/lab3_proto.py
+++ lab3/lab3_proto.py
@@ -1,5 +1,5 @@
 import numpy as np
-from lab3_tools import *
+from lab3.lab3_tools import *
 
 def words2phones(wordList, pronDict, addSilence=True, addShortPause=False):
     """ word2phones: converts word level to phone level transcription adding silence
