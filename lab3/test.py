b8c94bf223dac3b14e46a2bd189883115e0f0b17
pythonpsycho1337 <root@LAPTOP-V8QOMECQ.localdomain>
2018-05-01 14:12:41 +0200

    After merge

--- lab3/test.py
+++ lab3/test.py
@@ -1,10 +1,7 @@
 import lab3.lab3_tools as tools
-<<<<<<< HEAD
 import lab1.proto as lab1Proto
-=======
 
 tools.loadAudio("1a.wav")
->>>>>>> d2d51c390cfd61598ef9f5ab93bd3492ad3e6d0d
 
 samples, samplingrateR = tools.loadAudio("1a.wav")
 lmfcc = lab1Proto.mfcc(samples,samplingrate=samplingrateR)
