006da0b3593bc5288409c28af9aef0b9d3235825
yonk <nopctoday@gmail.com>
2018-05-04 15:55:11 +0200

    training data stacking completed

--- lab3/train.py
+++ lab3/train.py
@@ -1,13 +1,16 @@
 import numpy as np
 from keras.models import Sequential
 from keras.layers.core import Activation, Dense
+from lab3.StandardiseData import standardize_per_utterance, lmfcc_stack
+
 
 NUM_HIDDEN_LAYERS = 4 # number of hidden layers
 HIDDEN_U = 256
+NUM_STACK = 5
 
-data = np.load('lab3/train_data.npz')['data']
+data = standardize_per_utterance('lab3/train_data.npz')
 
-x = [d['lmfcc'] for d in data]
+x = [lmfcc_stack(d['lmfcc'], NUM_STACK) for d in data]
 x = np.vstack(x)
 
 model = Sequential()
