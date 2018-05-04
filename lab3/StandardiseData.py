013615d7129fb1715f4f0254b11f2ee3a7d121b2
yonk <nopctoday@gmail.com>
2018-05-04 14:51:46 +0200

    turned it into a function

--- lab3/StandardiseData.py
+++ lab3/StandardiseData.py
@@ -3,17 +3,19 @@ from sklearn.preprocessing import StandardScaler
 
 scaler = StandardScaler()
 
-a = np.load('G:/train_data.npz')['data']
 
 
-#Fitting each utterance
-standByUtterance = []
-for i in range(0,len(a)):
-    newDatapoint = {}
-    fittedScalar = scaler.fit(a[i]['lmfcc'])
-    newDatapoint['lmfcc'] = scaler.transform(a[i]['lmfcc'])
-    newDatapoint['targets'] = a[i]['targets'].copy()
-    standByUtterance.append(newDatapoint)
+def standardize_per_utterance(fname):
+    a = np.load(fname)['data']
+    #Fitting each utterance
+    standByUtterance = []
+    for i in range(0,len(a)):
+        newDatapoint = {}
+        fittedScalar = scaler.fit(a[i]['lmfcc'])
+        newDatapoint['lmfcc'] = scaler.transform(a[i]['lmfcc'])
+        newDatapoint['targets'] = a[i]['targets'].copy()
+        standByUtterance.append(newDatapoint)
+    return standByUtterance
 
 print("Done")
 
