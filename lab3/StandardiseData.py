4f4ba4d0ec8f3385dfd7773d7d2055d0de97b94c
pythonpsycho1337 <root@LAPTOP-V8QOMECQ.localdomain>
2018-05-04 14:57:38 +0200

    Minor bug fix

--- lab3/StandardiseData.py
+++ lab3/StandardiseData.py
@@ -1,21 +1,23 @@
 import numpy as np
 from sklearn.preprocessing import StandardScaler
 
-scaler = StandardScaler()
-
-
-
 def standardize_per_utterance(fname):
     a = np.load(fname)['data']
     #Fitting each utterance
+    scaler = StandardScaler()
     standByUtterance = []
     for i in range(0,len(a)):
         newDatapoint = {}
         fittedScalar = scaler.fit(a[i]['lmfcc'])
-        newDatapoint['lmfcc'] = scaler.transform(a[i]['lmfcc'])
+        newDatapoint['lmfcc'] = fittedScalar.transform(a[i]['lmfcc'])
         newDatapoint['targets'] = a[i]['targets'].copy()
         standByUtterance.append(newDatapoint)
     return standByUtterance
+
+def one_hot(target, maxNum):
+    pass
+
+stand = standardize_per_utterance("G:/train_data.npz")
 
 print("Done")
 
