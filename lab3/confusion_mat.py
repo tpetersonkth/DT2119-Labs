a70f8d6f07ea4d7dd4356fd45b374347ea8b45b0
yonk <nopctoday@gmail.com>
2018-05-14 18:04:06 +0200

    confusion matrix ready

--- lab3/confusion_mat.py
+++ lab3/confusion_mat.py
@@ -67,22 +67,22 @@ def plot_confusion_matrix(cm,
 
     if target_names is not None:
         tick_marks = np.arange(len(target_names))
-        plt.xticks(tick_marks, target_names, rotation=45)
-        plt.yticks(tick_marks, target_names)
+        plt.xticks(tick_marks, target_names, rotation=90, fontsize=5)
+        plt.yticks(tick_marks, target_names, fontsize=5)
 
     if normalize:
         cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
 
     thresh = cm.max() / 1.5 if normalize else cm.max() / 2
-    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
-        if normalize:
-            plt.text(j, i, "{:0.4f}".format(cm[i, j]),
-                     horizontalalignment="center",
-                     color="white" if cm[i, j] > thresh else "black")
-        else:
-            plt.text(j, i, "{:,}".format(cm[i, j]),
-                     horizontalalignment="center",
-                     color="white" if cm[i, j] > thresh else "black")
+    # for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+    #     if normalize:
+    #         plt.text(j, i, "{:0.4f}".format(cm[i, j]),
+    #                  horizontalalignment="center",
+    #                  color="white" if cm[i, j] > thresh else "black")
+    #     else:
+    #         plt.text(j, i, "{:,}".format(cm[i, j]),
+    #                  horizontalalignment="center",
+    #                  color="white" if cm[i, j] > thresh else "black")
 
     plt.tight_layout()
     plt.ylabel('True label')
--- lab3/evaluate.py
+++ lab3/evaluate.py
@@ -5,8 +5,12 @@ from keras.layers.core import Activation, Dense
 import matplotlib.pyplot as plt
 from StandardiseData import standardize_per_utterance, lmfcc_stack, standardize_per_training_set, get_training_and_validation_sets
 from confusion_mat import plot_confusion_matrix, get_confusion_matrix
+import pickle
 
 NUM_STACK = 5
+
+statlist = pickle.load(open('stateList.pkl', 'rb'))
+p = np.load('predicted_test.npy')
 
 train, validation = get_training_and_validation_sets(np.load('train_data.npz')['data'])
 _, __, test_data = standardize_per_training_set(train, [], np.load('test_data.npz')['data'])
@@ -16,12 +20,15 @@ x = [lmfcc_stack(d['lmfcc'], NUM_STACK) for d in test_data]
 x = np.vstack(x)
 y = [d['targets'] for d in test_data]
 y = np.hstack(y).T
-p = np.load('predicted_test.npy')
+
 
 predicted = p.argmax(axis=1)
 label = y.argmax(axis=1)
 cm = get_confusion_matrix(predicted, label)
-
+# manually normalize sil_0 and sil_1
+cm[39, 39] = 0
+cm[40, 40] = 0
+plot_confusion_matrix(cm, statlist)
 
 # model = keras.models.load_model('h3_adagrad_relu_u256_e100.h5py')
 # print('predicting..')
