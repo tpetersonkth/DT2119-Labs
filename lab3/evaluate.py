e16770e7b4bf4a1d798d164ec87992cb64fb4d8f
yonk <nopctoday@gmail.com>
2018-05-14 17:41:03 +0200

    confusion mat

--- lab3/evaluate.py
+++ lab3/evaluate.py
@@ -4,6 +4,7 @@ from keras.models import Sequential
 from keras.layers.core import Activation, Dense
 import matplotlib.pyplot as plt
 from StandardiseData import standardize_per_utterance, lmfcc_stack, standardize_per_training_set, get_training_and_validation_sets
+from confusion_mat import plot_confusion_matrix, get_confusion_matrix
 
 NUM_STACK = 5
 
@@ -14,10 +15,15 @@ print('processing data for input')
 x = [lmfcc_stack(d['lmfcc'], NUM_STACK) for d in test_data]
 x = np.vstack(x)
 y = [d['targets'] for d in test_data]
-y = np.vstack(y)
-p = np.load('predicted_test.npz')
+y = np.hstack(y).T
+p = np.load('predicted_test.npy')
 
-#model = keras.models.load_model('h3_adagrad_relu_u256_e100.h5py')
-print('predicting..')
-#p = model.predict(x)
+predicted = p.argmax(axis=1)
+label = y.argmax(axis=1)
+cm = get_confusion_matrix(predicted, label)
+
+
+# model = keras.models.load_model('h3_adagrad_relu_u256_e100.h5py')
+# print('predicting..')
+# p = model.predict(x)
 print('hello world')
